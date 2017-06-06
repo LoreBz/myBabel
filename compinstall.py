ff9fa7c575cd331163629588af36d09bf426f915
LoreBz <lorenzo.ghiro@gmail.com>
2017-06-06 15:39:54 +0200

    Project setup, a copy of official one from https://github.com/jech/babeld

---
+++ compinstall.py
@@ -0,0 +1,12 @@
+import os
+cmd = 'make'
+print cmd
+os.system(cmd)
+
+cmd = 'sudo make install'
+print cmd
+os.system(cmd)
+
+cmd = "sh cscoper.sh"
+print cmd
+os.system(cmd)
---
+++ cscoper.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+echo rebuilding cscope db
+find . -name "*.c" -o -name "*.cpp" -o -name "*.h" -o -name "*.hpp" > cscope.files
+cscope -q -R -b -i cscope.files
